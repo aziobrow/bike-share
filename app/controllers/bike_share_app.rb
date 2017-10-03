d0afa00f47258065951b23295627a79c40cd6450
Marshall Houston <marshall.houston@gmail.com>
2017-10-03 15:06:51 -0600

    add stations controller routes

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,38 @@
 class BikeShareApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
 
+  get "/" do
+    erb :dashboard
+  end
+
+  get "/stations" do
+    erb :index
+  end
+
+  get "/stations/new" do
+    erb :new
+  end
+
+  post "/stations" do
+    redirect "/stations"
+  end
+
+  get "/stations/:id" do
+    erb :show
+  end
+
+  get "/stations/:id/edit" do
+    erb :edit
+  end
+
+  set :method_override, true
+  put "/stations/:id" do |id|
+
+    redirect "/stations/#{id}"
+  end
+
+  delete "/stations/:id" do |id|
+
+    redirect "/stations"
+  end
 end
