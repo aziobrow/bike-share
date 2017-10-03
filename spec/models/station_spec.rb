547ace73699cfd4bd1ab44ffda09978d0419dc52
randy-darsh <rspringer47@gmail.com>
2017-10-03 14:51:09 -0600

    validation tests complete

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -6,5 +6,32 @@ RSpec.describe Station do
 
       expect(station).to_not be_valid
     end
+
+    it 'is invalid without a dock count' do
+      date = DateTime.now
+      station = Station.new(city: 'Place', name: 'Humberto', installation_date: date)
+
+      expect(station).to_not be_valid
+    end
+
+    it 'is invalid without a city' do
+      date = DateTime.now
+      station = Station.new(dock_count: 4, name: 'Humberto', installation_date: date)
+
+      expect(station).to_not be_valid
+    end
+
+    it 'is invalid without a installation_date' do
+      station = Station.new(dock_count: 4, name: 'Humberto', city: 'Place')
+
+      expect(station).to_not be_valid
+    end
+
+    it 'is valid with all attributes' do
+      date = DateTime.now
+      station = Station.new(installation_date: date, dock_count: 4, name: 'Humberto', city: 'Place')
+
+      expect(station).to be_valid
+    end
   end
 end
