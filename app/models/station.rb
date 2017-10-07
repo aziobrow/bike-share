554846344e421fe0f3fe26875224f674fa0fdd15
Aurora Ziobrowski <aziobrow@gmail.com>
2017-10-07 09:17:59 -0600

    started iteration 3

--- app/models/station.rb
+++ app/models/station.rb
@@ -3,4 +3,8 @@ class Station < ActiveRecord::Base
   validates :city, presence: true
   validates :dock_count, presence: true
   validates :installation_date, presence: true
+
+  def mean_number_of_bikes
+    Station.average(:dock_count)
+  end
 end
