66b0c3ddbfb64c164b057b3c62fedb2d979c5e55
randy-darsh <rspringer47@gmail.com>
2017-10-07 10:07:49 -0600

    add instance method tests for station

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -34,4 +34,41 @@ RSpec.describe Station do
       expect(station).to be_valid
     end
   end
+
+  describe 'Instance Methods' do
+    before :each do
+      date_1 = DateTime.now
+      date_2 = DateTime.new(2001,4,5,6,7)
+      @station_1 = Station.create(installation_date: date_1, dock_count: 4, name: 'Ralph', city: 'Place')
+      @station_2 = Station.create(installation_date: date_2, dock_count: 10, name: 'Humberto', city: 'Different Place')
+    end
+
+    it '#mean_number_of_bikes' do
+      expect(@station_1.mean_number_of_bikes).to eq(7)
+    end
+
+    it '#max_number_of_bikes' do
+      expect(@station_1.max_number_of_bikes).to eq(10)
+    end
+
+    it '#min_number_of_bikes' do
+      expect(@station_1.min_number_of_bikes).to eq(4)
+    end
+
+    it '#station_with_max_bikes' do
+      expect(@station_1.station_with_max_bikes.first.name).to eq("Humberto")
+    end
+
+    it '#station_with_min_bikes' do
+      expect(@station_1.station_with_min_bikes.first.name).to eq("Ralph")
+    end
+
+    it '#most_recently_installed_station' do
+      expect(@station_1.most_recently_installed_station.name).to eq("Ralph")
+    end
+
+    it '#oldest_station' do
+      expect(@station_1.oldest_station.name).to eq("Humberto")
+    end
+  end
 end
