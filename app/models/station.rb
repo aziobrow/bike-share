700d51d8d23e00e28d60ba1d47581c99b1e91056
Marshall Houston <marshall.houston@gmail.com>
2017-10-07 09:42:10 -0600

    add station-dashboard analysis methods

--- app/models/station.rb
+++ app/models/station.rb
@@ -7,4 +7,33 @@ class Station < ActiveRecord::Base
   def mean_number_of_bikes
     Station.average(:dock_count)
   end
+
+  def max_number_of_bikes
+    Station.maximum(:dock_count)
+  end
+
+  def station_with_max_bikes
+    Station.all.select do |station|
+      station.dock_count == max_number_of_bikes
+    end
+  end
+
+  def min_number_of_bikes
+    Station.minimum(:dock_count)
+  end
+
+  def station_with_min_bikes
+    Station.all.select do |station|
+      station.dock_count == min_number_of_bikes
+    end
+  end
+
+  def most_recently_installed_station
+    Station.order(:installation_date).last
+  end
+
+  def oldest_station
+    Station.order(:installation_date).first
+  end
+
 end
