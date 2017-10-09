562e3779ee0a102497a21e6b4150080560a30a27
Marshall Houston <marshall.houston@gmail.com>
2017-10-09 17:56:58 -0600

    add find_trips_in_a_range to condition model, wip

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -5,4 +5,35 @@ class Condition < ActiveRecord::Base
     date = Date.strptime(date, "%m/%d/%Y")
     find_by(date: date).id
   end
+
+  def self.find_highest_max_temp
+    maximum(:max_temperature)
+    .round(-1)
+  end
+
+  def self.find_lowest_max_temp
+    minimum(:max_temperature)
+    .floor(-1)
+  end
+
+  def self.find_trip_count_by_degree_range(range_floor)
+    condition_ids = where('? <= max_temperature AND ? >= max_temperature', range_floor, range_floor + 9)
+      .select(:id)
+
+    Trip.where('condition_id IN (?)', condition_ids).count
+
+    # Trip.where(condition_id:  )
+
+
+    # range_conditions.reduce(0) do |sum, condition|
+    #   sum + condition.trips.count
+    # end
+    #
+    # until range_floor > find_highest_max_temp
+    #   find_trip_count_by_degree_range(range_floor + 10)
+    # end
+  end
+
+  # select('conditions.*', )
+
 end
