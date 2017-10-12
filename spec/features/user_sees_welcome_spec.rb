57f5ad25c6c334e97e95a919531ad3db32b36445
randy-darsh <rspringer47@gmail.com>
2017-10-11 22:03:27 -0600

    fix welcome spec

--- spec/features/user_sees_welcome_spec.rb
+++ spec/features/user_sees_welcome_spec.rb
@@ -2,9 +2,14 @@ RSpec.describe "When a user visits the homepage" do
   it "they see a station index" do
     visit '/'
 
+    expect(page).to have_content('bike share')
+    expect(page).to have_content('home')
     expect(page).to have_content('stations')
     expect(page).to have_content('trips')
-    expect(page).to have_content('bike share')
+    expect(page).to have_content('weather conditions')
+    expect(page).to have_content('stations dashboard')
+    expect(page).to have_content('trips dashboard')
+    expect(page).to have_content('weather dashboard')
+    expect(page).to have_content('add a new...')
   end
-
 end
